cd892e4750f703192d5091d1dda4f52e528a4b76
belaka <creationforge@gmail.com>
2016-10-26 21:51:41 +0200

    Better values display

--- src/FullVibes/Bundle/BoardBundle/Controller/DashboardController.php
+++ src/FullVibes/Bundle/BoardBundle/Controller/DashboardController.php
@@ -8,20 +8,18 @@ class DashboardController extends Controller
 {
     public function indexAction()
     {
-        $temperature = $this->getAnalyticsManager()->getRepository()->findOneBy(array('eventKey' => 'temperature'), array('eventDate' => 'ASC'));
-        $humidity = $this->getAnalyticsManager()->getRepository()->findOneBy(array('eventKey' => 'humidity'), array('eventDate' => 'ASC'));
-        $air_quality = $this->getAnalyticsManager()->getRepository()->findOneBy(array('eventKey' => 'air_quality'), array('eventDate' => 'ASC'));
-        $moisture = $this->getAnalyticsManager()->getRepository()->findOneBy(array('eventKey' => 'moisture'), array('eventDate' => 'ASC'));
+        $keys = array('temperature','humidity','light','air_quality', 'moisture');
+        $lastEvents = $this->getAnalyticsManager()->getRepository()->findBy(array('eventKey' => $keys), array('eventDate' => 'DESC'), count($keys));
+        $data = [];
+        
+        foreach ($lastEvents as $analytic)  {
+            $data[$analytic->getEventKey()] = round($analytic->getEventValue(),2);
+            $date = $analytic->getEventDate();
+        }
         
         return $this->render(
                 'BoardBundle:Dashboard:index.html.twig',
-                array('data' => array(
-                    'temperature' => $temperature  ? round($temperature->getEventValue(),2) : 0, 
-                    'humidity' => $humidity ? round($humidity->getEventValue(),2) : 0, 
-                    'air_quality' => $air_quality ? round($air_quality->getEventValue(),2) : 0, 
-                    'moisture' => $moisture ? round($moisture->getEventValue(), 2) : 0
-                )
-            )
+                array('data' => $data, 'date' => $date)
         );
     }
     
