3611e83c5d405a49b9cb1e38fd6c1d66012eeb3d
belaka <creationforge@gmail.com>
2016-11-01 19:36:28 +0100

    Motor driver

--- src/FullVibes/Component/Actuator/MotorDriverActuator.php
+++ src/FullVibes/Component/Actuator/MotorDriverActuator.php
@@ -57,7 +57,7 @@ class MotorDriverActuator extends AbstractActuator {
     public function motorSpeedSetAB($motorSpeedA, $motorSpeedB) {
         //$speedA = $this->mapVals($motorSpeedA, 0, 100, 0, 255);
         //$speedB = $this->mapVals($motorSpeedB, 0, 100, 0, 255);
-        wiringPiI2CWriteBuffer (self::I2C_MOTOR_DRIVER_ADD, self::MOTOR_SPEED_SET, 0, $motorSpeedA, $motorSpeedB, 0, 4);
+        wiringPiI2CWriteBuffer (self::I2C_MOTOR_DRIVER_ADD, self::MOTOR_SPEED_SET, $motorSpeedA, $motorSpeedB, 0, 0, 4);
         //bus . write_i2c_block_data(self::I2C_MOTOR_DRIVER_ADD, self::MOTOR_SPEED_SET, []);
         sleep(.02);
     }
