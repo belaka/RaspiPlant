d7c62f663032d539cec13d4134bd22844a2ef760
belaka <creationforge@gmail.com>
2016-11-02 21:23:26 +0100

    loop on keys to collect datas

--- src/FullVibes/Bundle/BoardBundle/Controller/DashboardController.php
+++ src/FullVibes/Bundle/BoardBundle/Controller/DashboardController.php
@@ -8,7 +8,7 @@ class DashboardController extends Controller
 {
     public function indexAction()
     {
-        $keys = array('temperature_1','humidity_1','temperature_2','humidity_2', 'air_quality', 'moisture_1', 'moisture_2');
+        $keys = array('temperature_1','humidity_1','temperature_2','humidity_2', 'moisture_1', 'moisture_2', 'air_quality');
         $lastEvents = $this->getAnalyticsManager()->getRepository()->findBy(array('eventKey' => $keys), array('eventDate' => 'DESC'), count($keys));
         $data = [];
         
@@ -23,55 +23,28 @@ class DashboardController extends Controller
         $date = new \DateTime();
         $date->setTimezone(new \DateTimeZone('Europe/Paris'));
         $date->modify('-3 hours');
-        $temperatures = $this->getAnalyticsManager()->findByKeyAndDate('temperature_1', $date);
-        $tempArray = [];
         
-        foreach ($temperatures as $temperature) {
-            $tempValue = round($temperature->getEventValue(), 2);
-            if ($tempValue > 0 && $tempValue < 90) {
-                $tempArray[] = [addslashes($temperature->getEventDate()->format('H:i')), round($tempValue, 2)];
+        
+        $analytics = array();
+        
+        foreach ($keys as $key) {
+            $keyDatas = $this->getAnalyticsManager()->findByKeyAndDate($key, $date);
+            foreach ($keyDatas as $keyData) {
+                $keyValue = round($keyData->getEventValue(), 2);
+                if ($keyValue > 0 && $keyValue < 1000) {
+                    $analytics[$key][] = [addslashes($keyData->getEventDate()->format('H:i')), round($keyValue, 2)];
+                }
             }
         }
         
-        $humidities = $this->getAnalyticsManager()->findByKeyAndDate('humidity_1', $date);
-        $humArray = [];
+        //die(dump($analytics));
         
-        foreach ($humidities as $humidity) {
-            $humValue = round($humidity->getEventValue(), 2);
-            if ($humValue > 0 && $humValue < 90) {
-                $humArray[] = [addslashes($humidity->getEventDate()->format('H:i')), round($humValue, 2)];
-            }
-        }
-        
-        $moistures = $this->getAnalyticsManager()->findByKeyAndDate('moisture_1', $date);
-        $moistArray = [];
-        
-        foreach ($moistures as $moisture) {
-            $moistValue = round($moisture->getEventValue(), 2);
-            if ($moistValue > 0 && $moistValue < 1000) {
-                $moistArray[] = [addslashes($moisture->getEventDate()->format('H:i')), round($moistValue, 2)];
-            }
-        }
-        
-        $air_qualities = $this->getAnalyticsManager()->findByKeyAndDate('air_quality', $date);
-        $airArray = [];
-        
-        foreach ($air_qualities as $air_quality) {
-            $airValue = round($air_quality->getEventValue(), 2);
-            if ($airValue > 0 && $airValue < 500) {
-                $airArray[] = [addslashes($air_quality->getEventDate()->format('H:i')), round($airValue, 2)];
-            }
-        }
-               
         return $this->render(
                 'BoardBundle:Dashboard:index.html.twig',
                 array(
                     'data' => $data, 
-                    'date' => $date, 
-                    'temperatures' => json_encode($tempArray, JSON_UNESCAPED_SLASHES), 
-                    'humidities' => json_encode($humArray, JSON_UNESCAPED_SLASHES),
-                    'moistures' => json_encode($moistArray, JSON_UNESCAPED_SLASHES),
-                    'air_qualities' => json_encode($airArray, JSON_UNESCAPED_SLASHES),
+                    'date' => $date,
+                    'analytics' => json_encode($analytics, JSON_UNESCAPED_SLASHES)
                 )
         );
     }
