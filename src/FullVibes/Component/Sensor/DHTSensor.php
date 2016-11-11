95358838b99716dfa64793912780f1b2f5a729ae
belaka <creationforge@gmail.com>
2016-11-11 23:41:23 +0100

    DHTest

--- src/FullVibes/Component/Sensor/DHTSensor.php
+++ src/FullVibes/Component/Sensor/DHTSensor.php
@@ -52,7 +52,7 @@ class DHTSensor extends AbstractSensor {
             
             $this->device->digitalWrite(self::DHT_TEMP_CMD, $this->pin, $this->type, 0);
             usleep(18000);
-            $number = $this->device->readBuffer(0, $this->pin, 0, 32);
+            $number = $this->device->readBuffer(1, 0, 0, 32);
             $result = array_map ( function($val){return hexdec($val);} , explode(':', $number));
             
             $h = '';
