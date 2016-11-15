6e7e413e4779ff816ae5ee6cd302b05e8f519e1c
belaka <creationforge@gmail.com>
2016-11-16 00:52:37 +0100

    Added choice for state

--- src/FullVibes/Component/Form/Type/AtomizerFormType.php
+++ src/FullVibes/Component/Form/Type/AtomizerFormType.php
@@ -17,7 +17,15 @@ class AtomizerFormType extends AbstractType  {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('state', Type\CheckboxType::class)
+            ->add('state', Type\ChoiceType::class, array(
+                'choices' => array(
+                    0 => 'On',
+                    1 => 'Off'
+                ),
+                'expanded'  => true,
+                'multiple'  => false,
+                'data' => 1
+            ))
             ->add('save', Type\SubmitType::class)
         ;
     }
