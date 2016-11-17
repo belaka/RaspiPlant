219a49b1275a93e2f8c7c0e52784aeeb04b8df3b
belaka <creationforge@gmail.com>
2016-11-17 23:22:10 +0100

    Process output to get gpio table

--- src/FullVibes/Bundle/BoardBundle/Controller/GpioController.php
+++ src/FullVibes/Bundle/BoardBundle/Controller/GpioController.php
@@ -3,6 +3,8 @@
 namespace FullVibes\Bundle\BoardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use FullVibes\Component\WiringPi\WiringPi;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -25,29 +27,36 @@ class GpioController extends Controller
         $readall = array();
         
         $even = false;
-        exec('gpio readall', $readall);
-        
-        $response = "<pre>" . print_r($readall, 1) . "</pre>\n";
-        
-        for ($i = 3; $i < (count($readall) - 3); $i++) {
-            $row = explode('|', $readall[$i]);
-            $pin = intval(trim($row[1]));
-            if (in_array($pin, $pins)) {
-                $mode = trim($row[5]);
-                $value = trim($row[6]);
+        $process = new Process('ls -lsa');
+        $process->run();
 
-                $response .= '<tr class="' . (($even) ? 'even' : 'odd') . '">';
-                $response .= '<td>' . $pin . '</td>';
-                $response .= '<td>' . trim($row[2]) . '</td>';
-                $response .= '<td>' . trim($row[3]) . '</td>';
-                $response .= '<td>' . trim($row[4]) . '</td>';
-                $response .= '<td class="' . (($mode == 'IN') ? 'orange' : 'blue') . '"><a href="?c=pm&p=' . $pin . '&v=' . (($mode == 'IN') ? '1' : '0') . '">' . $mode . '</a></td>';
-                $response .= '<td class="' . (($value == 'High') ? 'green' : 'red') . '"><a href="?c=dw&p=' . $pin . '&v=' . (($value == 'High') ? '0' : '1') . '">' . $value . '</a></td>';
-                $response .= '</tr>';
-
-                $even = !$even;
-            }
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
         }
+
+        $gpioTable = $process->getOutput();
+        $response = "<pre>" . $gpioTable . "</pre>\n";
+        
+//        for ($i = 3; $i < (count($readall) - 3); $i++) {
+//            $row = explode('|', $readall[$i]);
+//            $pin = intval(trim($row[1]));
+//            if (in_array($pin, $pins)) {
+//                $mode = trim($row[5]);
+//                $value = trim($row[6]);
+//
+//                $response .= '<tr class="' . (($even) ? 'even' : 'odd') . '">';
+//                $response .= '<td>' . $pin . '</td>';
+//                $response .= '<td>' . trim($row[2]) . '</td>';
+//                $response .= '<td>' . trim($row[3]) . '</td>';
+//                $response .= '<td>' . trim($row[4]) . '</td>';
+//                $response .= '<td class="' . (($mode == 'IN') ? 'orange' : 'blue') . '"><a href="?c=pm&p=' . $pin . '&v=' . (($mode == 'IN') ? '1' : '0') . '">' . $mode . '</a></td>';
+//                $response .= '<td class="' . (($value == 'High') ? 'green' : 'red') . '"><a href="?c=dw&p=' . $pin . '&v=' . (($value == 'High') ? '0' : '1') . '">' . $value . '</a></td>';
+//                $response .= '</tr>';
+//
+//                $even = !$even;
+//            }
+//        }
         
         return $this->render(
                 'BoardBundle:Gpio:gpio.html.twig',
