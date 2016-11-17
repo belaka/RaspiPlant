cb104b746ffe0e101ddd9a73853e6773f61073fa
belaka <creationforge@gmail.com>
2016-11-17 23:51:59 +0100

    Better table handling [WIP]

--- src/FullVibes/Bundle/BoardBundle/Controller/GpioController.php
+++ src/FullVibes/Bundle/BoardBundle/Controller/GpioController.php
@@ -12,7 +12,7 @@ class GpioController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $pins = Array(0, 1, 2, 3, 4, 5, 6, 7, 17, 18, 19, 20);
+        //$pins = Array(0, 1, 2, 3, 4, 5, 6, 7, 17, 18, 19, 20);
         
         if (isset($_GET['c'])) {
                 if ($_GET['c'] == 'pm') {
@@ -32,28 +32,56 @@ class GpioController extends Controller
             throw new ProcessFailedException($process);
         }
 
-        $gpioTable = $process->getOutput();
-        $response = "<pre>" . $gpioTable . "</pre>\n";
+        $gpioTable = explode('\n', $process->getOutput());
         
-//        for ($i = 3; $i < (count($readall) - 3); $i++) {
+        $response = "<pre>" . print_r($gpioTable, 1) . "</pre>\n";
+        
+        $gpioArray = array();
+        
+        for ($i = 3; $i < (count($gpioArray) - 3); $i++) {
+            $response .= '<tr class="' . (($even) ? 'even' : 'odd') . '">';
+            
+            $row = array_map(function($v){ return trim($v);}, explode('|', $gpioArray[$i]));
 //            $row = explode('|', $readall[$i]);
 //            $pin = intval(trim($row[1]));
 //            if (in_array($pin, $pins)) {
 //                $mode = trim($row[5]);
 //                $value = trim($row[6]);
 //
-//                $response .= '<tr class="' . (($even) ? 'even' : 'odd') . '">';
-//                $response .= '<td>' . $pin . '</td>';
-//                $response .= '<td>' . trim($row[2]) . '</td>';
-//                $response .= '<td>' . trim($row[3]) . '</td>';
-//                $response .= '<td>' . trim($row[4]) . '</td>';
-//                $response .= '<td class="' . (($mode == 'IN') ? 'orange' : 'blue') . '"><a href="?c=pm&p=' . $pin . '&v=' . (($mode == 'IN') ? '1' : '0') . '">' . $mode . '</a></td>';
-//                $response .= '<td class="' . (($value == 'High') ? 'green' : 'red') . '"><a href="?c=dw&p=' . $pin . '&v=' . (($value == 'High') ? '0' : '1') . '">' . $value . '</a></td>';
-//                $response .= '</tr>';
+            $response .= '<tr class="' . (($even) ? 'even' : 'odd') . '">';
+            //BCM
+            $response .= '<td>' . trim($row[1]) . '</td>';
+            //Wpi
+            $response .= '<td>' . trim($row[2]) . '</td>';
+            //Name 
+            $response .= '<td>' . trim($row[3]) . '</td>';
+            //Mode
+            $response .= '<td>' . trim($row[4]) . '</td>';
+            //Value
+            $response .= '<td>' . trim($row[5]) . '</td>';
+            //Physical 
+            $response .= '<td>' . trim($row[6]) . '</td>';
+            //Physical 
+            $response .= '<td>' . trim($row[7]) . '</td>';
+            //Value
+            $response .= '<td>' . trim($row[8]) . '</td>';
+            //Mode
+            $response .= '<td>' . trim($row[9]) . '</td>';
+            //Name 
+            $response .= '<td>' . trim($row[10]) . '</td>';
+            //Wpi
+            $response .= '<td>' . trim($row[11]) . '</td>';
+            //BCM
+            $response .= '<td>' . trim($row[12]) . '</td>';
+            
+            
+            //$response .= '<td class="' . (($mode == 'IN') ? 'orange' : 'blue') . '"><a href="?c=pm&p=' . $pin . '&v=' . (($mode == 'IN') ? '1' : '0') . '">' . $mode . '</a></td>';
+            //$response .= '<td class="' . (($value == 'High') ? 'green' : 'red') . '"><a href="?c=dw&p=' . $pin . '&v=' . (($value == 'High') ? '0' : '1') . '">' . $value . '</a></td>';
+            $response .= '</tr>';
 //
 //                $even = !$even;
 //            }
-//        }
+        }
         
         return $this->render(
                 'BoardBundle:Gpio:gpio.html.twig',
