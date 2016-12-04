2f2e84ebebe9b3214c347cabd82ad691c6dd3d6e
belaka <creationforge@gmail.com>
2016-12-04 21:22:17 +0100

    Added button in actuator for simple control

--- src/FullVibes/Bundle/BoardBundle/Controller/ActuatorController.php
+++ src/FullVibes/Bundle/BoardBundle/Controller/ActuatorController.php
@@ -5,10 +5,11 @@ namespace FullVibes\Bundle\BoardBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use FullVibes\Component\Form\Type\AtomizerFormType;
-use FullVibes\Component\Actuator;
+use FullVibes\Bundle\BoardBundle\Entity\Actuator;
 use FullVibes\Component\Actuator\AbstractActuator;
 use FullVibes\Component\Device\I2CDevice;
 use FullVibes\Component\WiringPi\WiringPi;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ActuatorController extends Controller
 {
@@ -17,6 +18,28 @@ class ActuatorController extends Controller
         $actuators = $this->getActuatorManager()->findAll();
         
         return $this->render('BoardBundle:Actuator:index.html.twig', array('actuators' => $actuators));
+    }
+    
+    public function updateAction(Request $request, $id)
+    {
+        $actuatorManager = $this->getActuatorManager();
+        $actuator = $actuatorManager->find($id);
+
+        if (!($actuator instanceof Actuator)) {
+            throw new \Exception("Actuator not found.");
+        }
+        
+        $state = 0;
+        $checkbox = $request->request->get('checkbox');
+        
+        if ($checkbox === "on") {
+            $state = 1;
+        }
+        
+        $actuator->setState($state);
+        $actuatorManager->save($actuator);        
+        
+        return new JsonResponse(array('success' => true), 200);
     }
     
     /**
