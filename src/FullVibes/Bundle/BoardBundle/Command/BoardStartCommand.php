cb95ecc7e4462de160b253a2f2beffd4ba9cdb34
belaka <creationforge@gmail.com>
2016-12-04 13:43:00 +0100

    Retrieve actuator value

--- src/FullVibes/Bundle/BoardBundle/Command/BoardStartCommand.php
+++ src/FullVibes/Bundle/BoardBundle/Command/BoardStartCommand.php
@@ -18,11 +18,10 @@ use FullVibes\Component\WiringPi\WiringPi;
 class BoardStartCommand extends EndlessContainerAwareCommand {
 
     const ISO8601 = 'Y-m-d\TH:i:sP';
-    
+
     protected $debug;
-    
     protected $devices;
-    
+
     protected function configure() {
         $this->setName('raspiplant:board:start')
                 ->setDescription('Board loop start command.')
@@ -33,29 +32,28 @@ class BoardStartCommand extends EndlessContainerAwareCommand {
 
     // This is a normal Command::initialize() method and it's called exactly once before the first execute call
     protected function initialize(InputInterface $input, OutputInterface $output) {
-        
+
         $this->debug = false;
         $this->tick = 0;
-        
+
         /**
-        $airQualityPin = 2;
-        $moisturePin1 = 0;
-        $moisturePin2 = 1;
-        $atomizerPin = 5;
-        
-        $dhtPin = 6;
-        **/
-        
+          $airQualityPin = 2;
+          $moisturePin1 = 0;
+          $moisturePin2 = 1;
+          $atomizerPin = 5;
+
+          $dhtPin = 6;
+         * */
         $boards = $this->getBoardManager()->findAll();
-        
-        if (!$boards)  {
+
+        if (!$boards) {
             throw new \Exception("No boards found...");
         }
-        
+
         foreach ($boards as $board) {
             $this->boardInitialize($board, $output);
         }
-        
+
         $output->writeln([
             '=======================================================================',
             '========================   RASPIPLANT START    ========================',
@@ -63,113 +61,130 @@ class BoardStartCommand extends EndlessContainerAwareCommand {
             php_uname(),
         ]);
     }
-    
+
     protected function boardInitialize(Board $board, $output) {
-        
+
         $output->writeln("Starting Board :" . $board->getName());
-        
+
         $devices = $board->getDevices();
-        
+
         foreach ($devices as $device) {
             $this->devices[$device->getId()] = $this->deviceInitialize($device, $output);
             usleep(100000);
         }
     }
-    
+
     protected function deviceInitialize(Device $device, $output) {
-        
+
         $output->writeln("Starting Device :" . $device->getName());
-        
+
         $fd = WiringPi::wiringPiI2CSetup($device->getAddress());
         $class = $device->getClass();
         $deviceLink = new $class($fd);
         
+        if (!($deviceLink instanceof DeviceInterface)) {
+            throw new \Exception("Initialization error of device: " . $class . " with address " . $device->getAddress());
+        }
+
         $sensors = $device->getSensors();
         foreach ($sensors as $sensor) {
-            $this->devices[$device->getId()]['sensors'][] = $this->sensorInitialize($sensor, $deviceLink, $output);
+            $this->devices[$device->getId()]['sensors'][$sensor->getId()] = $this->sensorInitialize($sensor, $deviceLink, $output);
         }
-        
+
         $actuators = $device->getActuators();
         foreach ($actuators as $actuator) {
-            $this->devices[$device->getId()]['actuators'][] = $this->actuatorInitialize($actuator, $deviceLink, $output);
+            $this->devices[$device->getId()]['actuators'][$actuator->getId()] = $this->actuatorInitialize($actuator, $deviceLink, $output);
         }
     }
-    
+
     protected function actuatorInitialize(Actuator $actuator, $deviceLink, $output) {
-        
+
         $output->writeln("Starting Actuator :" . $actuator->getName());
-        
+
         $class = $actuator->getClass();
         $a = new $class($deviceLink, $actuator->getPin());
-        
+
         if (!($a instanceof ActuatorInterface)) {
             throw new \Exception("Initialization error of actuator: " . $class);
         }
-        
+
         usleep(100000);
-        
+
         return $a;
     }
-    
+
     protected function sensorInitialize(Sensor $sensor, $deviceLink, $output) {
-        
+
         $output->writeln("Starting Sensor :" . $sensor->getName());
-        
+
         $class = $sensor->getClass();
         $s = new $class($deviceLink, $sensor->getPin());
-        
-        if (!($a instanceof ActuatorInterface)) {
+
+        if (!($s instanceof SensorInterface)) {
             throw new \Exception("Initialization error of sensor: " . $class);
         }
-        
+
         usleep(100000);
-        
+
         return $s;
     }
-    
+
     protected function execute(InputInterface $input, OutputInterface $output) {
 
         $firedAt = new \DateTime();
-        
+
         $data = array();
-        foreach ($this->devices as $id => $device)  {
+
+        foreach ($this->devices as $id => $device) {
+
             $output->writeln("# Starting Read of sensors values at " . $firedAt->format(self::ISO8601));
+
             $data[$id] = $this->readDeviceSensors($device['sensors']);
+
             $output->writeln("# Starting set of actuators values at " . $firedAt->format(self::ISO8601));
+
             $this->setDeviceActuators($device['actuators']);
         }
-        
-        if (($this->tick % 120) == 0) {    
+
+        if (($this->tick % 120) == 0) {
             foreach ($data as $deviceData) {
                 $this->persistValues($deviceData, $firedAt, $output);
             }
         }
 
         $output->writeln(print_r($data, 1));
-        
+
         $this->tick += 10;
     }
-    
+
     protected function readDeviceSensors(array $sensors) {
+
         $values = array();
-        foreach ($sensors as $sensor) {
-            $values[] = json_decode($sensor->readSensorData());
+
+        foreach ($sensors as $sensorId => $sensor) {
+            $values[$sensorId] = json_decode($sensor->readSensorData());
             usleep(100000);
         }
+
         return $values;
     }
-    
+
     protected function setDeviceActuators(array $actuators) {
-        foreach ($actuators as $actuator) {
+
+        $actuatorManager = $this->getActuatorManager();
+
+        foreach ($actuators as $actuatorId => $actuator) {
+
             //We get the value from database
-            
-            $actuator->writeSatus();
+            $actuatorData = $actuatorManager->findOne($actuatorId);
+
+            $actuator->writeSatus($actuatorData->getValue());
             usleep(100000);
         }
     }
 
     protected function persistValues($data, $firedAt, $output) {
-        
+
         $analyticsManager = $this->getAnalyticsManager();
         $output->writeln("Data added to database " . $firedAt->format(self::ISO8601));
 
@@ -178,7 +193,7 @@ class BoardStartCommand extends EndlessContainerAwareCommand {
             $analyticsManager->save($analytics);
         }
     }
-    
+
     /**
      * 
      * @return \FullVibes\Bundle\BoardBundle\Manager\AnalyticsManager
@@ -186,6 +201,15 @@ class BoardStartCommand extends EndlessContainerAwareCommand {
     protected function getBoardManager() {
 
         return $this->getContainer()->get("board.manager.board_manager");
+    }
+
+    /**
+     * 
+     * @return \FullVibes\Bundle\BoardBundle\Manager\AnalyticsManager
+     */
+    protected function getActuatorManager() {
+
+        return $this->getContainer()->get("board.manager.actuator_manager");
     }
 
     /**
@@ -215,9 +239,5 @@ class BoardStartCommand extends EndlessContainerAwareCommand {
         // Keep it short! We may need to exit because the OS wants to shutdown
         // and we can get killed if it takes to long!
     }
-    
-    /*
-    
-     */
 
 }
